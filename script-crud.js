5299201530f88456ab478f47335b750e9bb7abaa
Alexandre Costa <alexandre.costa@furg.br>
2025-12-31 15:40:51 -0300

    adicionar novos ícones e implementar funcionalidade para adicionar tarefas

---
+++ script-crud.js
@@ -0,0 +1,24 @@
+// Encontrar o botão adicionar tarefa
+
+const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
+const formAdicionarTarefa = document.querySelector(".app__form-add-task");
+const textarea = document.querySelector(".app__form-textarea");
+
+const tarefas = [];
+
+btnAdicionarTarefa.addEventListener("click", function () {
+  formAdicionarTarefa.classList.toggle("hidden");
+});
+
+formAdicionarTarefa.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const descricaoTarefa = textarea.value;
+  const tarefa = {
+    descricao: textarea.value,
+  };
+
+  tarefas.push(tarefa);
+
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+});
